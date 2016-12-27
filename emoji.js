5330d7dd75362e4d85dc8a2a57e2dd4945d2de93
Giorgi Parunov <g.parunov@singular.uk>
2016-12-28 02:48:48 +0400

    Converting image to 8bit added

--- emoji.js
+++ emoji.js
@@ -6,6 +6,21 @@ var Emoji = (function () {
         if (this.picture)
             this.setImage(this.picture);
     }
+    Emoji.prototype.eightBit = function (scale) {
+        /**
+         * Draws a pixelated version of an image in a given canvas
+         * @param {number} scale - the scale factor: between 0 and 100
+         */
+        scale *= 0.01;
+        var scaledW = this.canvas.width * scale;
+        var scaledH = this.canvas.height * scale;
+        var context = this.canvas.getContext('2d');
+        context.mozImageSmoothingEnabled = false;
+        context.webkitImageSmoothingEnabled = false;
+        context.imageSmoothingEnabled = false;
+        context.drawImage(this.image, 0, 0, scaledW, scaledH);
+        context.drawImage(this.canvas, 0, 0, scaledW, scaledH, 0, 0, this.image.width, this.image.height);
+    };
     Emoji.prototype.setImage = function (picture) {
         /**
          * Set Image
@@ -31,6 +46,7 @@ var Emoji = (function () {
          * @return {Array} Objects with colors , x and y positions
          */
         var canvas = this.canvas, context = this.context;
+        this.eightBit(2);
         var pixels = context.getImageData(0, 0, canvas.width, canvas.height).data;
         var pixelColors = [];
         for (var g = 0, y = 0, x = 0; g < pixels.length; g += 4) {
@@ -54,6 +70,27 @@ var Emoji = (function () {
     };
     Emoji.prototype.changeCanvasPixel = function () {
         var canvas = this.canvas, context = this.context;
+        var testObj = {};
+        console.log('start');
+        for (var i = 0, x = 0, y = 0; i < 500; i++) {
+            console.log(i);
+            var testArray = [];
+            if (x % this.canvas.width == 0) {
+                x = 0;
+                y = y + 2;
+            }
+            else {
+                x = x + 2;
+            }
+            testObj = {
+                firstColor: context.getImageData(x, y, canvas.width, canvas.height).data,
+                secondColor: context.getImageData(x + 1, y, canvas.width, canvas.height).data,
+                thirdColor: context.getImageData(x, y + 1, canvas.width, canvas.height).data,
+                fourthColor: context.getImageData(x + 1, y + 1, canvas.width, canvas.height).data,
+            };
+        }
+        console.log('end');
+        console.log(testObj);
     };
     return Emoji;
 }());
--- emoji.ts
+++ emoji.ts
@@ -1,8 +1,14 @@
-class Emoji {
+interface IEmoji {
+    picture: string;
+    canvas: any;
+    context: any;
+}
+
+class Emoji implements IEmoji{
 
     public picture : string;
-    private canvas;
-    private context;
+    public canvas;
+    public context;
     public image;
 
     constructor(picture: string) {
@@ -10,6 +16,28 @@ class Emoji {
         this.canvas = document.createElement('canvas');
         this.context = this.canvas.getContext('2d');
         if (this.picture) this.setImage(this.picture);
+    }
+
+    eightBit(scale : number) {
+        /**
+         * Draws a pixelated version of an image in a given canvas
+         * @param {number} scale - the scale factor: between 0 and 100
+         */
+
+        scale *= 0.01;
+
+        let scaledW = this.canvas.width * scale;
+        let scaledH = this.canvas.height * scale;
+
+        let context = this.canvas.getContext('2d');
+
+        context.mozImageSmoothingEnabled = false;
+        context.webkitImageSmoothingEnabled = false;
+        context.imageSmoothingEnabled = false;
+
+        context.drawImage(this.image, 0, 0, scaledW, scaledH);
+        context.drawImage(this.canvas, 0, 0, scaledW, scaledH, 0, 0, this.image.width, this.image.height);
+
     }
 
     setImage(picture) {
@@ -20,6 +48,7 @@ class Emoji {
         this.image = new Image();
 
         document.body.appendChild(this.canvas);
+
 
         let emoji = this;
 
@@ -33,7 +62,9 @@ class Emoji {
                 this.onImageLoad();
             }
         };
+
         this.image.src = picture;
+
 
     }
 
@@ -44,6 +75,9 @@ class Emoji {
          */
         let canvas = this.canvas,
             context = this.context;
+
+        this.eightBit(2);
+
 
         let pixels = context.getImageData(0, 0, canvas.width, canvas.height).data;
 
@@ -71,8 +105,30 @@ class Emoji {
     changeCanvasPixel() {
         let canvas = this.canvas,
             context = this.context;
+        let testObj : any = {};
+
+        console.log('start');
+        for(let i = 0 , x = 0 , y = 0 ; i < 500; i++) {
+            console.log(i);
+            let testArray : any[] = [];
+            if(x % this.canvas.width == 0) {
+                x = 0;
+                y = y + 2;
+            } else {
+                x = x + 2;
+            }
+
+            testObj = {
+                firstColor : context.getImageData( x , y , canvas.width , canvas.height).data,
+                secondColor: context.getImageData( x + 1 , y , canvas.width , canvas.height).data,
+                thirdColor : context.getImageData( x , y + 1 , canvas.width , canvas.height).data,
+                fourthColor: context.getImageData(x + 1 , y + 1 , canvas.width , canvas.height).data,
+            }
+
+        }
+        console.log('end');
+        console.log(testObj);
+
     }
-
-
 
 }
