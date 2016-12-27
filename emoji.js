4051482dd51eaef99d35db93430b9517cefd57fb
Giorgi Parunov <g.parunov@singular.uk>
2016-12-27 19:44:53 +0400

    Changed Information from array of color to the object of colors x and y positions

--- emoji.js
+++ emoji.js
@@ -1,16 +1,19 @@
 var Emoji = (function () {
     function Emoji(picture) {
         this.picture = picture;
-        this.canvas = document.getElementById("canvas");
+        this.canvas = document.createElement('canvas');
         this.context = this.canvas.getContext('2d');
         if (this.picture)
             this.setImage(this.picture);
     }
     Emoji.prototype.setImage = function (picture) {
+        /**
+         * Set Image
+         * @return {String} image
+         */
         this.image = new Image();
+        document.body.appendChild(this.canvas);
         var emoji = this;
-        // canvas aრ მისცემს საიტს უფლებას რომ getImageData გამოიყენო ფოტოზე რომელიც
-        // სხვა საიტიდან წამოიღე, ამიტომ ეს ატრიბუტი უნდა
         this.image.setAttribute("crossOrigin", "Anonymous");
         this.image.onload = function () {
             emoji.canvas.width = this.width;
@@ -23,17 +26,34 @@ var Emoji = (function () {
         this.image.src = picture;
     };
     Emoji.prototype.getCanvasColors = function () {
+        /**
+         * Get Picture all pixel`s colors
+         * @return {Array} Objects with colors , x and y positions
+         */
         var canvas = this.canvas, context = this.context;
         var pixels = context.getImageData(0, 0, canvas.width, canvas.height).data;
         var pixelColors = [];
-        for (var g = 0; g < pixels.length; g += 4) {
-            pixelColors.push(pixels[g] + ',' + pixels[g + 1] + ',' + pixels[g + 2]);
+        for (var g = 0, y = 0, x = 0; g < pixels.length; g += 4) {
+            if (g % this.canvas.width == 0) {
+                y++;
+                x = 1;
+            }
+            else
+                x++;
+            var color = pixels[g] + ',' + pixels[g + 1] + ',' + pixels[g + 2];
+            var posX = x;
+            var posY = y;
+            var pixelInfo = {
+                color: color,
+                xPosition: posX,
+                yPosition: posY
+            };
+            pixelColors.push(pixelInfo);
         }
-        console.log(pixelColors);
+        return pixelColors;
+    };
+    Emoji.prototype.changeCanvasPixel = function () {
+        var canvas = this.canvas, context = this.context;
     };
     return Emoji;
 }());
-var test = new Emoji("https://scontent-vie1-1.xx.fbcdn.net/v/t31.0-8/15591476_1383178805056939_3259992289809107891_o.jpg?oh=f4031aef100ff4a282ab32d8fab6d821&oe=58F79006");
-window.onload = function () {
-    test.getCanvasColors();
-};
--- emoji.ts
+++ emoji.ts
@@ -7,20 +7,22 @@ class Emoji {
 
     constructor(picture: string) {
         this.picture = picture;
-
-        this.canvas = document.getElementById("canvas");
+        this.canvas = document.createElement('canvas');
         this.context = this.canvas.getContext('2d');
-
         if (this.picture) this.setImage(this.picture);
     }
 
     setImage(picture) {
+        /**
+         * Set Image
+         * @return {String} image
+         */
         this.image = new Image();
+
+        document.body.appendChild(this.canvas);
 
         let emoji = this;
 
-        // canvas aრ მისცემს საიტს უფლებას რომ getImageData გამოიყენო ფოტოზე რომელიც
-        // სხვა საიტიდან წამოიღე, ამიტომ ეს ატრიბუტი უნდა
         this.image.setAttribute("crossOrigin", "Anonymous");
         this.image.onload = function(){
             emoji.canvas.width = this.width;
@@ -36,25 +38,41 @@ class Emoji {
     }
 
     getCanvasColors() {
+        /**
+         * Get Picture all pixel`s colors
+         * @return {Array} Objects with colors , x and y positions
+         */
         let canvas = this.canvas,
             context = this.context;
 
         let pixels = context.getImageData(0, 0, canvas.width, canvas.height).data;
 
-        let pixelColors: string[] = [];
+        let pixelColors: any[] = [];
 
-        for(let g = 0 ; g < pixels.length ; g += 4) {
-            pixelColors.push(pixels[g] + ',' + pixels[g + 1] + ',' + pixels[g + 2]);
+        for(let g = 0 , y = 0 , x = 0; g < pixels.length ; g += 4) {
+            if(g % this.canvas.width == 0) {
+                y++;
+                x = 1;
+            } else x++;
+            let color: string = pixels[g] + ',' + pixels[g + 1] + ',' + pixels[g + 2];
+            let posX : number = x ;
+            let posY : number = y ;
+            let pixelInfo : any = {
+              color: color,
+              xPosition : posX,
+              yPosition: posY
+            };
+            pixelColors.push(pixelInfo);
         }
 
-        console.log(pixelColors);
-
+        return pixelColors;
     }
 
+    changeCanvasPixel() {
+        let canvas = this.canvas,
+            context = this.context;
+    }
+
+
+
 }
-
-const test:Emoji = new Emoji("https://scontent-vie1-1.xx.fbcdn.net/v/t31.0-8/15591476_1383178805056939_3259992289809107891_o.jpg?oh=f4031aef100ff4a282ab32d8fab6d821&oe=58F79006");
-
-window.onload = () =>  {
-    test.getCanvasColors();
-};
