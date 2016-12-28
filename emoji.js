62c6111da70739e8e7852bb5ebb848b638366006
Ika Pkhakadze <i.pkhakadze@gmail.com>
2016-12-28 16:54:07 +0400

    pixlize image and get colors

--- emoji.js
+++ emoji.js
@@ -1,30 +1,31 @@
 var Emoji = (function () {
     function Emoji(picture) {
+        this.emojiSize = 30;
         this.picture = picture;
         this.canvas = document.createElement('canvas');
         this.context = this.canvas.getContext('2d');
         if (this.picture)
             this.setImage(this.picture);
     }
+    /**
+    * Draws a pixelated version of an image in a given canvas
+    * @param {number} scale - the scale factor: between 0 and 100
+    */
     Emoji.prototype.eightBit = function (scale) {
-        /**
-         * Draws a pixelated version of an image in a given canvas
-         * @param {number} scale - the scale factor: between 0 and 100
-         */
         scale *= 0.01;
-        var scaledW = this.canvas.width * scale;
-        var scaledH = this.canvas.height * scale;
+        var scaledW = Math.floor(this.canvas.width / this.emojiSize);
+        var scaledH = Math.floor(this.canvas.height / this.emojiSize);
         this.context.mozImageSmoothingEnabled = false;
         this.context.webkitImageSmoothingEnabled = false;
         this.context.imageSmoothingEnabled = false;
         this.context.drawImage(this.image, 0, 0, scaledW, scaledH);
         this.context.drawImage(this.canvas, 0, 0, scaledW, scaledH, 0, 0, this.image.width, this.image.height);
     };
+    /**
+     * Set Image
+     * @return {String} image
+    */
     Emoji.prototype.setImage = function (picture) {
-        /**
-         * Set Image
-         * @return {String} image
-         */
         this.image = new Image();
         document.body.appendChild(this.canvas);
         var emoji = this;
@@ -39,32 +40,38 @@ var Emoji = (function () {
         };
         this.image.src = picture;
     };
+    /**
+     * Get Picture all pixel`s colors
+     * @return {Array} Objects with colors , x and y positions
+     */
     Emoji.prototype.getCanvasColors = function () {
-        /**
-         * Get Picture all pixel`s colors
-         * @return {Array} Objects with colors , x and y positions
-         */
         var canvas = this.canvas, context = this.context;
+        var rectW = this.emojiSize;
         this.eightBit(1);
         var pixels = context.getImageData(0, 0, canvas.width, canvas.height).data;
         var pixelColors = [];
-        for (var g = 0, y = 0, x = 0; g < pixels.length; g += 50) {
-            if (g % this.canvas.width == 0) {
-                y += 50;
-                x = 1;
+        var step = 4;
+        var xPos = 0;
+        var yPos = 0;
+        do {
+            var index = (xPos + yPos * canvas.width) * 4;
+            console.log('%c ' + xPos + ' ' + yPos, 'background: rgb(' + pixels[index + 0] + ',' + pixels[index + 1] + ',' + pixels[index + 2] + ')');
+            pixelColors.push({
+                point: {
+                    x: xPos,
+                    y: yPos
+                },
+                color: { r: pixels[index + 0], g: pixels[index + 1], b: pixels[index + 2] }
+            });
+            xPos += rectW;
+            if (xPos >= canvas.width) {
+                yPos += rectW;
+                xPos = 1;
             }
-            else
-                x++;
-            var color = pixels[g] + ',' + pixels[g + 1] + ',' + pixels[g + 2];
-            var posX = x;
-            var posY = y;
-            var pixelInfo = {
-                color: color,
-                xPosition: posX,
-                yPosition: posY
-            };
-            pixelColors.push(pixelInfo);
-        }
+            if (yPos >= canvas.height) {
+                break;
+            }
+        } while (true);
         return pixelColors;
     };
     Emoji.prototype.changeCanvasPixel = function () {
@@ -80,3 +87,8 @@ var Emoji = (function () {
     };
     return Emoji;
 }());
+function run() {
+    var url = '/scarlet.png';
+    var emoji = new Emoji(url);
+    setTimeout(function () { emoji.getCanvasColors(); }, 1000);
+}
--- emoji.ts
+++ emoji.ts
@@ -4,12 +4,14 @@ interface IEmoji {
     context: any;
 }
 
-class Emoji implements IEmoji{
+class Emoji implements IEmoji {
 
-    public picture : string;
+    public picture: string;
     public canvas;
     public context;
     public image;
+
+    public emojiSize = 30;
 
     constructor(picture: string) {
         this.picture = picture;
@@ -18,16 +20,19 @@ class Emoji implements IEmoji{
         if (this.picture) this.setImage(this.picture);
     }
 
-    eightBit(scale : number) {
-        /**
-         * Draws a pixelated version of an image in a given canvas
-         * @param {number} scale - the scale factor: between 0 and 100
-         */
+    /**
+    * Draws a pixelated version of an image in a given canvas
+    * @param {number} scale - the scale factor: between 0 and 100
+    */
+    eightBit(scale: number) {
+
 
         scale *= 0.01;
 
-        let scaledW = this.canvas.width * scale;
-        let scaledH = this.canvas.height * scale;
+
+
+        let scaledW = Math.floor(this.canvas.width / this.emojiSize);
+        let scaledH = Math.floor(this.canvas.height / this.emojiSize);
 
         this.context.mozImageSmoothingEnabled = false;
         this.context.webkitImageSmoothingEnabled = false;
@@ -38,11 +43,12 @@ class Emoji implements IEmoji{
 
     }
 
+    /**
+     * Set Image
+     * @return {String} image
+    */
     setImage(picture) {
-        /**
-         * Set Image
-         * @return {String} image
-         */
+
         this.image = new Image();
 
         document.body.appendChild(this.canvas);
@@ -51,7 +57,7 @@ class Emoji implements IEmoji{
         let emoji = this;
 
         this.image.setAttribute("crossOrigin", "Anonymous");
-        this.image.onload = function(){
+        this.image.onload = function () {
             emoji.canvas.width = this.width;
             emoji.canvas.height = this.height;
             emoji.context.drawImage(this, 0, 0, this.width, this.height);
@@ -65,13 +71,16 @@ class Emoji implements IEmoji{
 
     }
 
+    /**
+     * Get Picture all pixel`s colors
+     * @return {Array} Objects with colors , x and y positions
+     */
     getCanvasColors() {
-        /**
-         * Get Picture all pixel`s colors
-         * @return {Array} Objects with colors , x and y positions
-         */
         let canvas = this.canvas,
             context = this.context;
+
+
+        var rectW = this.emojiSize;
 
         this.eightBit(1);
 
@@ -80,21 +89,38 @@ class Emoji implements IEmoji{
 
         let pixelColors: any[] = [];
 
-        for(let g = 0 , y = 0 , x = 0; g < pixels.length ; g += 50) {
-            if(g % this.canvas.width == 0) {
-                y += 50;
-                x = 1;
-            } else x++;
-            let color: string = pixels[g] + ',' + pixels[g + 1] + ',' + pixels[g + 2];
-            let posX : number = x ;
-            let posY : number = y ;
-            let pixelInfo : any = {
-              color: color,
-              xPosition : posX,
-              yPosition: posY
-            };
-            pixelColors.push(pixelInfo);
-        }
+
+        let step = 4;
+        let xPos = 0;
+        let yPos = 0;
+
+        do {
+
+            let index = (xPos + yPos * canvas.width) * 4;
+            console.log('%c ' + xPos + ' ' + yPos, 'background: rgb(' + pixels[index + 0] + ',' + pixels[index + 1] + ',' + pixels[index + 2] + ')')
+
+            pixelColors.push({
+                point: {
+                    x: xPos,
+                    y: yPos
+                },
+                color: { r: pixels[index + 0], g: pixels[index + 1], b: pixels[index + 2] }
+            })
+
+            xPos += rectW;
+
+            if (xPos >= canvas.width) {
+                yPos += rectW;
+                xPos = 1;
+            }
+
+            if (yPos >= canvas.height) {
+                break;
+            }
+
+
+
+        } while (true);
 
         return pixelColors;
     }
@@ -102,11 +128,11 @@ class Emoji implements IEmoji{
     changeCanvasPixel() {
         let canvas = this.canvas,
             context = this.context;
-        let testObj : any = {};
+        let testObj: any = {};
 
-        let test : number = 1;
+        let test: number = 1;
         console.log('start');
-        for(let i = 0; i < this.getCanvasColors().length; i += 50) {
+        for (let i = 0; i < this.getCanvasColors().length; i += 50) {
             console.log(test++);
         }
         console.log('end');
@@ -114,4 +140,12 @@ class Emoji implements IEmoji{
 
     }
 
+}
+
+
+function run() {
+    var url = '/scarlet.png';
+    var emoji = new Emoji(url);
+
+    setTimeout(function () { emoji.getCanvasColors(); }, 1000)
 }
