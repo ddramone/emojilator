26763ca49a52f036bc82a9d77141cd4e7d899a2f
Giorgi Parunov <g.parunov@singular.uk>
2016-12-29 13:11:10 +0400

    changes var to let

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { Scene } from './scene';
 let scene = new Scene({ mosaicSize: 16 });
 
 scene.loadImage(
-    './public/scarlet.png',
+    'https://scontent-fra3-1.xx.fbcdn.net/v/t31.0-8/15000220_1444436535584079_6723146236336182812_o.jpg?oh=217bc63f8222c2cd76c513fdcf005585&oe=58EEAECD',
     function () {
 
         scene.mosaic();
